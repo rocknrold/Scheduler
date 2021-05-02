6924c3a9ff71a7733a566de0576aa0852d98df1c
rocknrold <bsitharold@gmail.com>
2021-05-02 20:08:29 +0800

    alpha 1.2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
-    {{-- <meta name="google-signin-client_id" content="583439515778-ip59hm3jlhqg1hm3vbcdme433mcu0irj.apps.googleusercontent.com"> --}}
     <meta name="google-signin-client_id" content="583439515778-ip59hm3jlhqg1hm3vbcdme433mcu0irj.apps.googleusercontent.com">
+    {{-- <meta name="google-signin-client_id" content="454455940038-q8kdrp7b3um8dimq4adus1sucfilr35p.apps.googleusercontent.com"> --}}
 
 
     <!-- CSRF Token -->
@@ -63,74 +63,52 @@
 
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Scheduler') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{-- ADD USER AVATAR BEHIND NAME --}}
-                                    @if(Auth::user()->type == "google")
-                                        <img src="{{ Auth::user()->avatar }}" style="width:32px; height:32px; position:left; top:10px; left:10px; border-radius:50%">
-                                    @else
-                                        <img src="/storage/{{ Auth::user()->avatar }}" style="width:32px; height:32px; position:left; top:10px; left:10px; border-radius:50%">
-                                    @endif
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    @if(Auth::user()->type == "sched")
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                        document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>
-                                    @elseif(Auth::user()->type == "google")
-                                        <a class="dropdown-item" href="#" onclick="signOut();">Sign out</a>
-                                    @endif
-                                </div>
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Scheduler') }}
+            </a>
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
+                        @endif
+
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                </li>
+                    @else
+                <li class="nav-item" style="padding-left:10px;">
+                        @if(Auth::user()->type == "google")
+                            <img src="{{ Auth::user()->avatar }}" style="width:32px; height:32px; position:left; top:10px; left:10px; border-radius:50%">
+                        @endif
+                        {{ Auth::user()->name }}
+                       |
+                        @if(Auth::user()->type == "sched")
+                            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }} <i class="fas fa-sign-out-alt"></i>
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        @elseif(Auth::user()->type == "google")
+                            <a href="#" onclick="signOut();">Sign out</a>
+                        @endif
+                    @endguest
+                </li>
+            </ul>                       
         </nav>
             <main>
                 @yield('content')
             </main>
-    </div>
     {{-- <main> --}}
     @yield('home')
     {{-- </main> --}}
+    </div>
 
     {{-- YIELDS SCRIPTS --}}
     @yield('scripts')
